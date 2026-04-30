e0782969735640b027880cce794f7f4ae2da8405
Fridi63 <friditer@gmail.com>
2026-04-30 20:40:13 +0300

    local changes before pull

--- app/routers/pages.py
+++ app/routers/pages.py
@@ -7,14 +7,17 @@ router = APIRouter()
 _TEMPL = Path(__file__).resolve().parent.parent.parent / "templates"
 templates = Jinja2Templates(directory=str(_TEMPL))
 
+
 @router.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse(request,"index.html", {"title": "Каталог"})
+    return templates.TemplateResponse(request, "index.html", {"title": "Каталог"})
+
 
 @router.get("/login", response_class=HTMLResponse)
 def login_page(request: Request):
-    return templates.TemplateResponse(request,"login.html", {"title": "Вход"})
+    return templates.TemplateResponse(request, "login.html", {"title": "Вход"})
+
 
 @router.get("/admin/users", response_class=HTMLResponse)
 def users_page(request: Request):
-    return templates.TemplateResponse(request,"users.html", {"title": "Пользователи"})
+    return templates.TemplateResponse(request, "users.html", {"title": "Пользователи"})
--- app/seed.py
+++ app/seed.py
@@ -1,64 +1,79 @@
 from decimal import Decimal
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import Category, Product, User, UserRole
 from app.security import get_password_hash
-from app.models import User, UserRole, Product, Category
+
 
 async def seed_if_empty(session: AsyncSession) -> None:
     user_exists = await session.scalar(select(User.id).limit(1))
     if user_exists is None:
         session.add_all(
             [
-                User(username="admin", password=get_password_hash("qwerty123"), role=UserRole.ADMIN),
-                User(username="advanced", password=get_password_hash("qwerty123"), role=UserRole.ADVANCED),
-                User(username="simple", password=get_password_hash("qwerty123"), role=UserRole.SIMPLE),
-            ])
+                User(
+                    username="admin",
+                    password=get_password_hash("qwerty123"),
+                    role=UserRole.ADMIN,
+                ),
+                User(
+                    username="advanced",
+                    password=get_password_hash("qwerty123"),
+                    role=UserRole.ADVANCED,
+                ),
+                User(
+                    username="simple",
+                    password=get_password_hash("qwerty123"),
+                    role=UserRole.SIMPLE,
+                ),
+            ]
+        )
 
     category_exists = await session.scalar(select(Category.id).limit(1))
     if category_exists is None:
-        food = Category(name='Еда')
-        sweets = Category(name='Вкусности')
-        water = Category(name='Вода')
+        food = Category(name="Еда")
+        sweets = Category(name="Вкусности")
+        water = Category(name="Вода")
         session.add_all([food, sweets, water])
         await session.flush()
 
     product_exists = await session.scalar(select(Product.id).limit(1))
     if product_exists is None:
         categories = await session.execute(select(Category))
-        categories_name = {c.name: c.id for c in categories.scalars().all()}
+        categories_by_name = {c.name: c.id for c in categories.scalars().all()}
         session.add_all(
             [
                 Product(
-                    name='Селедка',
+                    name="Селедка",
                     description="Селедка соленая",
-                    price=Decimal(10.000),
-                    general_note='Акция',
-                    special_note='Пересоленая',
-                    category_id=categories_name['Еда'],
+                    price=Decimal("10.0000"),
+                    general_note="Акция",
+                    special_note="Пересоленая",
+                    category_id=categories_by_name["Еда"],
                 ),
                 Product(
-                    name='Тушенка',
+                    name="Тушенка",
                     description="Тушенка говяжья",
-                    price=Decimal(20.000),
-                    general_note='Вкусная',
-                    special_note='Жилы',
-                    category_id=categories_name['Еда'],
+                    price=Decimal("20.0000"),
+                    general_note="Вкусная",
+                    special_note="Жилы",
+                    category_id=categories_by_name["Еда"],
                 ),
                 Product(
-                    name='Сгущенка',
+                    name="Сгущенка",
                     description="В банках",
-                    price=Decimal(30.000),
-                    general_note='С ключом',
-                    special_note='Вкусная',
-                    category_id=categories_name['Вкусности'],
+                    price=Decimal("30.0000"),
+                    general_note="С ключом",
+                    special_note="Вкусная",
+                    category_id=categories_by_name["Вкусности"],
                 ),
                 Product(
-                    name='Квас',
+                    name="Квас",
                     description="В бутылках",
-                    price=Decimal(15.000),
-                    general_note='Вятский',
-                    special_note='Теплый',
-                    category_id=categories_name['Вода'],
+                    price=Decimal("15.0000"),
+                    general_note="Вятский",
+                    special_note="Теплый",
+                    category_id=categories_by_name["Вода"],
                 ),
             ]
         )
